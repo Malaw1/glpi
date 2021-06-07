4ee582df183324006e54db0d0268b8cb35b212d3
Djibril NDIAYE <djibsone51@gmail.com>
2021-06-07 15:44:56 +0000

    Sending files

---
+++ config/config_db.php
@@ -0,0 +1,7 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost     = 'localhost';
+   public $dbuser     = 'lab01';
+   public $dbpassword = 'Lab01db_dk';
+   public $dbdefault  = 'glpidb';
+}
